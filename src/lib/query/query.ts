80cd29cdc39384cc16a33d6f1018db5571cd2254
HyunSJ <dudtod1596@gmail.com>
2022-07-25 01:03:29 +0900

    add queries for data fetching

---
+++ src/lib/query/query.ts
@@ -0,0 +1,51 @@
+import { gql } from "@apollo/client";
+
+export const searchCampDetailById = gql`
+    query searchCAmpDetailById($contentId: Int){
+        findCampById(contentId: $contentId) {
+            
+        }
+    }
+`;
+
+export const searchAllCamps = gql`
+  query searchAllCamps($first: Int) {
+    searchCamps(first: $first) {
+      contentId
+      firstImageUrl # 대표 이미지
+      doNm # 도
+      sigunguNm # 시군구
+      facltNm # 야영장명
+      lineIntro # 한줄소개
+      addr1 # 주소
+      tel # 연락처
+      sbrsCl # 부대시설
+    }
+  }
+`;
+
+export const searchCampsByQuery = gql`
+  query searchAllCamps($first: Int, $after: Int, $params: CampSearchParamsDto) {
+    searchCamps(first: $first, after: $after, params: $params) {
+      contentId
+      firstImageUrl # 대표 이미지
+      doNm # 도
+      sigunguNm # 시군구
+      facltNm # 야영장명
+      lineIntro # 한줄소개
+      addr1 # 주소
+      tel # 연락처
+      sbrsCl # 부대시설
+    }
+  }
+`;
+export const SearchAnimalThemeCampsForCarousal = gql`
+  query searchAllCamps($first: Int, $params: CampSearchParamsDto) {
+    searchCamps(first: $first, params: $params) {
+      contentId
+      firstImageUrl # 대표 이미지
+      facltNm # 야영장명
+      lineIntro # 한줄소개
+    }
+  }
+`;
