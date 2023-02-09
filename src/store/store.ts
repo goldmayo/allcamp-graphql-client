6e2a71d191d80706c4cfdf877ca4142b41230ad0
HyunSJ <dudtod1596@gmail.com>
2023-02-10 01:32:01 +0900

    Add : store에 상세검색 슬라이스 추가

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,40 @@
+import { AdvancedSearchParamsInterface } from "@/types/searchFormType";
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-interface Store {
-  items: { id: number; name: string }[];
+interface SearchParamsStore {
+  AdvancedSearchParams: AdvancedSearchParamsInterface;
+  setSearchParams: (name: string, value: string) => void;
+  selectSearchParams: (name: string, value: string) => void;
+  deleteSearchParams: (name: string, value: string) => void;
+  resetSearchParams: (name: string, value: string) => void;
 }
 
-const initialState: Store = {
-  items: [],
+const InitialAdvancedSearchParams: AdvancedSearchParamsInterface = {
+  region: [],
+  campsite: [],
+  location: [],
+  theme: [],
+  facility: [],
+  operation: [],
+  floor: [],
+  etcinfo: [],
 };
+
+const useSearchParamsStore = create<SearchParamsStore>((set) => ({
+  AdvancedSearchParams: InitialAdvancedSearchParams,
+  setSearchParams: (name, value) =>
+    set((state) => ({ AdvancedSearchParams: { ...state.AdvancedSearchParams, [name]: [value] } })),
+  selectSearchParams: (name, value) =>
+    set((state) => ({
+      AdvancedSearchParams: { ...state.AdvancedSearchParams, [name]: [...state.AdvancedSearchParams[name], value] },
+    })),
+  deleteSearchParams: (name, value) =>
+    set((state) => ({
+      AdvancedSearchParams: {
+        ...state.AdvancedSearchParams,
+        [name]: state.AdvancedSearchParams[name].filter((el) => el != value),
+      },
+    })),
+  resetSearchParams: () => set((state) => ({ AdvancedSearchParams: { ...InitialAdvancedSearchParams } })),
+}));
